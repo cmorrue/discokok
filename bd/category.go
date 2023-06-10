32f09bfe6346b691d33ee71e9e56a04622ff334c
cmorrue <carmorrue@gmail.com>
2023-06-10 16:38:59 +0200

    Ultimo Commit

--- bd/category.go
+++ bd/category.go
@@ -3,13 +3,14 @@ package bd
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
+	"strings"
 
 	//"strconv"
-	//"strings"
 
 	"github.com/cmorrue/discok/models"
+	"github.com/cmorrue/discok/tools"
 	_ "github.com/go-sql-driver/mysql"
-	//"github.com/cmorrue/discok/tool"
 )
 
 func InsertCategory(c models.Category) (int64, error) {
@@ -37,5 +38,41 @@ func InsertCategory(c models.Category) (int64, error) {
 
 	fmt.Println("Insert Category > Ejecución Exitosa")
 	return LastInsertId, nil
+
+}
+
+func UpdateCategory(c models.Category) error {
+
+	fmt.Println("Comienza registro de UpdateCategory")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "UPDATE category SET "
+
+	if len(c.CategName) > 0 {
+		sentencia += " Categ_Name = '" + tools.EscapeString(c.CategName) + "'"
+	}
+
+	if len(c.CategPath) > 0 {
+		if !strings.HasPrefix(sentencia, "SET") {
+			sentencia += ", "
+		}
+		sentencia += "Categ_Path = '" + tools.EscapeString(c.CategPath) + "'"
+	}
+
+	sentencia += " WHERE Categ_Id = " + strconv.Itoa(c.CategID)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Update Category > Ejecución Exitosa")
+	return nil
 
 }
